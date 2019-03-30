d3a0751a5217398d0e53aa63989a2ab3a94d65a3
rahiii <rahimitranyc@gmail.com>
2019-03-30 14:38:33 +0000

    warm up day 6

--- warmupDay6.py
+++ warmupDay6.py
@@ -1,1 +1,15 @@
 # File name: warmupDay6.py
+number = int(input("Guess the secret number!"))
+while number != 9:
+    print("Try again")
+    number = int(input("(Hint: It is a odd number) "))
+print("You guessed right!")
+
+lettern = int(input("Choose a value for n "))
+letterm = int(input("Choose a value for m "))
+print( lettern, " to the power of ", letterm, " is ", lettern**letterm )
+
+guess = 10
+while guess != -1:
+    print(guess)
+    guess -= 1
